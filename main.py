548fc87d48f34948cb885d34e977405bbe169091
Ayu <y.abouseada16@gmail.com>
2021-04-08 17:07:56 +0200

    V0.1

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#Conditional Logic
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print('you are old enough to drive and you have  a license')
+# elif is_licenced:
+#     print('You have license')
+else:
+    print('You cant drive')
+
+print('nice')
